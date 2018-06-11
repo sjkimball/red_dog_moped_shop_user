8089d5d81b9a917a52e8dd1d6a40d89bb35c64b6
Sam Kimball <s.j.kimball@icloud.com>
2018-06-11 15:37:15 -0500

    Add Nav component to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header/header.js';
+import Nav from './components/navigation/nav/nav.js';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="l-container">
         <Header />
+        <Nav />
       </div>
     );
   }
