fce07ef9ad4c07448fbd56e88c4fcb3d315cf360
Sam Kimball <s.j.kimball@icloud.com>
2018-06-11 16:00:58 -0500

    Initial styleguidist settings

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,19 @@
 module.exports = {
+  ignore: ['**/components/navigation/nav/nav.js'],
+  template: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://use.typekit.net/ius6gqc.css'
+        }
+      ]
+    }
+  },
+  theme: {
+    fontFamily: {
+      base: '"Industry", sans-serif'
+    }
+  },
   components: 'src/components/**/**/*.js'
 };
