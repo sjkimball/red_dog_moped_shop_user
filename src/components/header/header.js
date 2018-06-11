c498519e1ebe9f06c7263d52967fcdad145ecad5
Sam Kimball <s.j.kimball@icloud.com>
2018-06-11 14:04:22 -0500

    Update from class to className

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,10 @@ import "./header.css";
 
 let Header = (props) => {
   return (
-    <header class="header">
-        <h1 class="header__heading header__heading--white">
-          <a href="/">Red D<span class="header__heading header__heading--red">ø</span>g M<span class="header__heading header__heading--red">ø</span>ped</a></h1>
+    <header className="header">
+        <h1 className="header__heading header__heading--white">
+          <a href="/">Red D<span className="header__heading header__heading--red">ø</span>g M<span className="header__heading header__heading--red">ø</span>ped</a>
+        </h1>
     </header>
   );
 }
