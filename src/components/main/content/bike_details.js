1b04fb16432aa6f4a52b48c4c6495db6ffe6e41a
Sam Kimball <s.j.kimball@icloud.com>
2018-06-14 16:10:08 -0500

    Add styling to header pics

--- src/components/main/content/bike_details.js
+++ src/components/main/content/bike_details.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './bike_details.css';
 
+import diablo from './../../../images/Indigan-Diablo.jpg';
+import qrcode from './../../../images/sampleqr.png';
+
 class BikeDetails extends React.Component {
   constructor(props) {
     super(props);
@@ -12,6 +15,7 @@ class BikeDetails extends React.Component {
     }
   }
   render () {
+    // const diablo = require('../../../images/Indigan-Diablo.jpg');
     return(
       <div className="main__container">
 
@@ -22,42 +26,61 @@ class BikeDetails extends React.Component {
           Washington, D.C.
 
         </p>
-          <img src="/public/images/Indigan-Diablo.jpg" alt="Indigan Diablo" className="main__header-pic"/>
-        <div className="main__header-qrcode">QRCode</div>
+        <img src={diablo} alt="Indigan Diablo" className="main__header-pic" />
+        <img src={qrcode} alt="Sample QR Code" className="main__header-qrcode" />
+
       </div>
 
       <div className="main__supporting--repair">
 
-        <form>
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="bike-onsite" />
-            <label class="form-check-label" for="bike-onsite">
-              Bike Received?
-            </label>
-          </div>
-          <div class="form-group">
-            <label for="repair-time">Quoted Repair Time:</label>
-            <textarea class="form-control" id="repair-time" placeholder="e.g. '2 - 3 days'" />
-          </div>
-          <div class="form-group">
-            <label for="repair-cost">Quoted Repair Cost:</label>
-            <textarea class="form-control" id="repair-cost" placeholder="e.g. '$150'" />
+        <form className="supporting__form">
+
+          <div className="supporting__form--left">
+
+            <div className="form-group">
+              <label htmlFor="repair-issue">Issue:</label>
+              <textarea className="form-control" id="repair-issue" placeholder="Customer description" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="repair-time">Quoted Repair Time:</label>
+              <textarea className="form-control" id="repair-time" placeholder="e.g. '2 - 3 days'" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="repair-cost">Quoted Repair Cost:</label>
+              <textarea className="form-control" id="repair-cost" placeholder="e.g. '$150'" />
+            </div>
+            <div className="form-check supporting__form--right">
+              <input className="form-check-input" type="checkbox" value="" id="bike-onsite" />
+              <label className="form-check-label" htmlFor="bike-onsite">
+                Bike Received?
+              </label>
+            </div>
+
           </div>
 
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="bike-rfp" />
-            <label class="form-check-label" for="bike-rfp">
-              Ready for pickup?
-            </label>
-          </div>
-          <div class="btn-group">
-            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Repair Status
-            </button>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">Awaiting Service</a>
-              <a class="dropdown-item" href="#">In Service</a>
-              <a class="dropdown-item" href="#">Pending</a>
+          <div className="supporting__form--right hidden">
+            <div className="form__repair-status">
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="bike-rfp" />
+                <label className="form-check-label" htmlFor="bike-rfp">
+                  Add to Repair Queue?
+                </label>
+              </div>
+
+              <div className="btn-group">
+                <button type="button" className="btn btn-danger dropdown-toggle repair-status" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Repair Status
+                </button>
+                <div className="dropdown-menu">
+                  <a className="dropdown-item" href="#">Awaiting Service</a>
+                  <a className="dropdown-item" href="#">In Service</a>
+                  <a className="dropdown-item" href="#">Pending</a>
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="repair-cost">Add Repair comment</label>
+              <textarea className="form-control" id="repair-cost" placeholder="e.g. Replaced starter" />
             </div>
           </div>
         </form>
