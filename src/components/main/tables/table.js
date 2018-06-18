fd7fbdc77706120aabb4a788adc78ccef28bc2ab
Sam Kimball <jibequan@gmail.com>
2018-06-18 03:03:40 -0500

    Why why why why will this not work

--- src/components/main/tables/table.js
+++ src/components/main/tables/table.js
@@ -6,6 +6,32 @@ import "react-table/react-table.css";
 
 import base from '../../../base.js'
 
+let getBikes = (bidAray) => {
+  let repairBikes = [];
+  for (let i = 0; i < bidAray.length; i++) {
+    base.fetch('bikes', {
+      context: this,
+      asArray: false,
+      queries: {
+        orderByChild: 'bike_Id',
+        equalTo: `${bidAray[i]}`
+      }
+    }).then((data) => {
+        console.log("Why", data);
+        repairBikes.push(data);
+      })
+    }
+    console.log("test", repairBikes);
+    return repairBikes;
+}
+
+let getBikeIds = (aRay) => {
+  const bikeaRay = aRay.map(item => {
+    return item.bike_Id;
+  });
+  return bikeaRay;
+}
+
 class PrimaryTable extends React.Component {
   constructor(props){
     super(props);
@@ -22,47 +48,38 @@ class PrimaryTable extends React.Component {
       state: 'repairs',
       asArray: true,
       then() {
-        this.setState({ loading: false });
-      }
-    });
-
-    this.ref = base.syncState('bikes', {
-      context: this,
-      state: 'bikes',
-      asArray: true,
-      then() {
-        this.setState({ loading: false });
+        this.setState({
+          bikes: getBikes(getBikeIds(this.state.repairs)),
+          loading: false
+         });
       }
     });
   }
 
- getBikeRepairData = (aRay, twoRay) => {
-
- }
-
   render() {
-    const repairs = this.state.repairs;
     const bikes = this.state.bikes;
-    const repair_bikes = [];
+    const repairs = this.state.repairs;
+    console.log(bikes);
+    console.log(repairs);
 
     return (
         <ReactTable
-          data={repairs}
+          data={bikes}
           columns = {[
             {
               Header: "Awaiting Triage",
               columns: [
                 {
-                  Header: "Repair ID",
-                  accessor: "repair_Id"
-                },
-                {
                   Header: "Bike ID",
                   accessor: "bike_Id"
                 },
                 {
-                  Header: "User Id",
-                  accessor: "uid"
+                  Header: "Model",
+                  accessor: "model"
+                },
+                {
+                  Header: "Year",
+                  accessor: "year"
                 }
               ]
             }
